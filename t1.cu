bb6882ca13fa015a6a7b75aa8463af57ed0218f3
Albi <csipakalbert0@gmail.com>
2023-04-10 17:41:06 +0200

    files added

---
+++ t1.cu
@@ -0,0 +1,136 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+#define N 10
+#define K 2
+
+#define Threads 2000
+#define Block_Size 5
+
+int Block_Count = (N - 1) / Block_Size + 1;
+
+int numbers[N] = {1,3,5,6,9,10,12,14,16,18};
+int pos = -1;
+
+__device__ int d_numbers[N];
+__device__ int d_pos = -1;
+
+//van e benne K hoszzu számtani sorozat
+//cpun
+//gpun 1 szállal
+//gpun sok szállal
+//shared memoryval
+//blokkok n>1024
+
+void CPU() {
+	pos = -1;
+	for (size_t i = 0; i < N-K; i++)
+	{
+		int counter = 0;
+		while (counter < K && numbers[i + counter] + 2 == numbers[i + counter + 1]) {
+			counter++;
+		}
+		if (counter == K) {
+			pos = i;
+		}
+	}
+}
+
+__global__ void SingleThread() {
+	d_pos = -1;
+	for (size_t i = 0; i < N - K; i++)
+	{
+		int counter = 0;
+		while (counter < K && d_numbers[i + counter] + 2 == d_numbers[i + counter + 1]) {
+			counter++;
+		}
+		if (counter == K) {
+			d_pos = i;
+		}
+	}
+}
+
+__global__ void MultiThread() {
+	d_pos = -1;
+	int counter = 0;
+	while (counter < K && d_numbers[threadIdx.x + counter]+2 == d_numbers[threadIdx.x+counter +1]) {
+		counter++;
+	}
+	if (counter == K) {
+		d_pos = threadIdx.x;
+	}
+
+}
+
+__global__ void MultiThreadShared() {
+	d_pos = -1;
+	__shared__ int s_numbers[N];
+	
+	if (threadIdx.x==0)
+	{
+		for (size_t i = 0; i < N; i++)
+		{
+			s_numbers[i] = d_numbers[i];
+		}
+	}
+	__syncthreads();
+
+	int counter = 0;
+	while (counter < K && s_numbers[threadIdx.x + counter] + 2 == s_numbers[threadIdx.x + counter + 1]) {
+		counter++;
+	}
+	if (counter == K) {
+		d_pos = threadIdx.x;
+	}
+
+}
+
+__global__ void MultiThreadWithBlocks() {
+	d_pos = -1;
+
+	int counter = 0;
+	while (counter < K && d_numbers[blockIdx.x * blockDim.x + threadIdx.x + counter] + 2 == d_numbers[blockIdx.x * blockDim.x + threadIdx.x + counter + 1]) {
+		counter++;
+	}
+	if (counter == K) {
+		d_pos = blockIdx.x * blockDim.x + threadIdx.x;
+	}
+
+}
+
+int main() {
+
+	for (size_t i = 0; i < N; i++)
+	{
+		printf("[%d]=%d\n",i, numbers[i]);
+	}
+
+	cudaMemcpyToSymbol(d_numbers, numbers, N*sizeof(int));
+
+	CPU();
+	printf("\nCPU: A sorozat a %d. elemtol kezdodik", pos);
+	pos = -1;
+
+	SingleThread << <1, 1 >> > ();
+	cudaMemcpyFromSymbol(&pos,d_pos,sizeof(int));
+	printf("\nSingleThread: A sorozat a %d. elemtol kezdodik", pos);
+	pos = -1;
+
+	MultiThread << <1, N - K + 1 >> > ();
+	cudaMemcpyFromSymbol(&pos, d_pos, sizeof(int));
+	printf("\nMultiThread: A sorozat a %d. elemtol kezdodik", pos);
+	pos = -1;
+
+	MultiThreadShared << <1, N-K+1>> > ();
+	cudaMemcpyFromSymbol(&pos, d_pos, sizeof(int));
+	printf("\nMultiThreadShared: A sorozat a %d. elemtol kezdodik", pos);
+	pos = -1;
+
+	MultiThreadWithBlocks << <Block_Count, Block_Size >> > ();
+	cudaMemcpyFromSymbol(&pos, d_pos, sizeof(int));
+	printf("\nMultiThreadWithBlocks: A sorozat a %d. elemtol kezdodik", pos);
+	pos = -1;
+
+	cudaGetLastError();
+}
---
+++ t2.cu
@@ -0,0 +1,206 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+//occupancy = 67%%
+
+//n tömb k konstans 
+
+//1. feladat: melyik k hosszu rész sorozat összege a legkisebb ,összeg és index 
+//2. feladat shared memory
+//3. feladat mi az ideális blokkméret - max blokkméret lekérdezése
+//4. feladat: ciklussal időmérés
+//5. feladat occupancy calculatorral ->hatékonyság 
+
+#define N 10
+#define K 3
+
+int numbers[N] = { 1,5,1,6,9,0,1,3,1,1 };
+int pos;
+int minSum;
+
+__device__ int d_numbers[N];
+__device__ int d_pos;
+__device__ int d_minSum;
+
+void CPU() {
+	pos = -1;
+	minSum = 100;
+	int counter = 0;
+	int tmpSum = 0;
+	for (size_t i = 0; i < N-K; i++)
+	{
+		counter = 0;
+		tmpSum = 0;
+		while (counter < K) {
+			tmpSum += numbers[counter+i];
+			counter++;
+			;
+		}
+		if (tmpSum < minSum)
+		{
+			pos = i;
+			minSum = tmpSum;
+		}
+	}
+}
+
+__global__ void SingleThread() {
+	d_pos = -1;
+	d_minSum = 100;
+	int counter = 0;
+	int tmpSum = 0;
+	for (size_t i = 0; i < N - K; i++)
+	{
+		counter = 0;
+		tmpSum = 0;
+		while (counter < K) {
+			tmpSum += d_numbers[counter + i];
+			counter++;
+			;
+		}
+		if (tmpSum < d_minSum)
+		{
+			d_pos = i;
+			d_minSum = tmpSum;
+		}
+	}
+}
+
+__global__ void MultiThread() {
+	d_pos = -1;
+	d_minSum = 100;
+
+	int counter = 0;
+	int tmpSum = 0;
+	while (counter < K) {
+		tmpSum += d_numbers[counter + threadIdx.x];
+		counter++;
+	}
+	atomicMin(&d_minSum, tmpSum);
+
+	if (d_minSum == tmpSum)
+	{
+		d_pos = threadIdx.x;
+	}
+}
+
+__global__ void MultiThreadShared() {
+	d_pos = -1;
+	d_minSum = 100;
+	__shared__ int s_numbers[N];
+	__shared__ int s_minSum;
+	int counter = 0;
+	int tmpSum = 0;
+
+	if (threadIdx.x == 0)
+	{
+		for (size_t i = 0; i < N; i++)
+		{
+			s_numbers[i] = d_numbers[i];
+		}
+		s_minSum = d_minSum;
+	}
+	__syncthreads();
+
+	while (counter < K) {
+		tmpSum += s_numbers[counter + threadIdx.x];
+		counter++;
+	}
+	
+	atomicMin(&s_minSum, tmpSum);
+
+	if (s_minSum == tmpSum)
+	{
+		d_pos = threadIdx.x;
+		d_minSum = s_minSum;
+	}
+
+}
+
+__global__ void MultiThreadWithBlocks() {
+	d_pos = -1;
+	d_minSum = 100;
+	__shared__ int s_numbers[N];
+	__shared__ int s_minSum;
+	int counter = 0;
+	int tmpSum = 0;
+
+	if (blockIdx.x * blockDim.x + threadIdx.x == 0)
+	{
+		for (size_t i = 0; i < N; i++)
+		{
+			s_numbers[i] = d_numbers[i];
+		}
+		s_minSum = d_minSum;
+	}
+	__syncthreads();
+
+	while (counter < K) {
+		tmpSum += s_numbers[counter + blockIdx.x * blockDim.x + threadIdx.x];
+		counter++;
+	}
+
+	atomicMin(&s_minSum, tmpSum);
+
+	if (s_minSum == tmpSum)
+	{
+		d_pos = blockIdx.x * blockDim.x + threadIdx.x;
+		d_minSum = s_minSum;
+	}
+}
+
+int main() {
+
+	for (size_t i = 0; i < N; i++)
+	{
+		printf("[%d]=%d\n", i, numbers[i]);
+	}
+
+	//ideális blokkméret
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+	printf("\nMax Threads Per Block: %d", prop.maxThreadsPerBlock);
+	printf("\nMax Blocks: %d", prop.maxBlocksPerMultiProcessor);
+
+	//cpu
+	CPU();
+	printf("\n\nCPU: A sorozat a %d. elemtol kezdodik, osszege: %d", pos,minSum);
+
+	//Single Thread
+	cudaMemcpyToSymbol(d_numbers, numbers, N * sizeof(int)); //cpy to gpu
+	SingleThread << <1, 1 >> > ();
+	cudaMemcpyFromSymbol(&minSum, d_minSum, sizeof(int)); //cpy back
+	cudaMemcpyFromSymbol(&pos, d_pos, sizeof(int)); //cpy back
+	printf("\nSingleThread: A sorozat a %d. elemtol kezdodik, osszege: %d", pos, minSum);
+
+	//MultiThread
+	cudaMemcpyToSymbol(d_numbers, numbers, N * sizeof(int)); //cpy to gpu
+	MultiThread << <1, N - K  >> > ();
+	cudaMemcpyFromSymbol(&minSum, d_minSum, sizeof(int)); //cpy back
+	cudaMemcpyFromSymbol(&pos, d_pos, sizeof(int)); //cpy back
+	printf("\nMultiThread: A sorozat a %d. elemtol kezdodik, osszege: %d", pos, minSum);
+
+	//MultiThreadShared
+	cudaMemcpyToSymbol(d_numbers, numbers, N * sizeof(int)); //cpy to gpu
+	MultiThreadShared << <1, N - K >> > ();
+	cudaMemcpyFromSymbol(&minSum, d_minSum, sizeof(int)); //cpy back
+	cudaMemcpyFromSymbol(&pos, d_pos, sizeof(int)); //cpy back
+	printf("\nMultiThreadShared: A sorozat a %d. elemtol kezdodik, osszege: %d\n", pos, minSum);
+
+	//MultiThreadWithBlocks
+	for (size_t i = 1; i <= prop.maxBlocksPerMultiProcessor; i++)
+	{
+		int Block_Count = (N - 1) /i + 1;
+
+		cudaMemcpyToSymbol(d_numbers, numbers, N * sizeof(int)); //cpy to gpu
+		MultiThreadWithBlocks << <Block_Count, N-K >> > ();
+		cudaMemcpyFromSymbol(&minSum, d_minSum, sizeof(int)); //cpy back
+		cudaMemcpyFromSymbol(&pos, d_pos, sizeof(int)); //cpy back
+		printf("\nMultiThreadWithBlocks: A sorozat a %d. elemtol kezdodik, osszege: %d, blocksize: %d", pos, minSum,i);
+	}
+
+	printf("\n");
+
+	cudaGetLastError();
+}
